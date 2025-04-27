a268009925bb39e0b1b38ac34cb3ebf55226b4e4
Egor Kolesnik <egorisk1998@gmail.com>
2025-04-27 23:59:25 +0300

    decided to restart, confused myself too much

--- scripts.js
+++ scripts.js
@@ -1,13 +1,1 @@
-const body = document.querySelector("body");
-const container = document.createElement("div");
-container.className = "container";
-body.appendChild(container);
-
-function createGrid(rowNum, collumnNum) {
-    for (let i = 0; i < rowNum * collumnNum; i++) {
-        const div = document.createElement("div");
-        div.className = "grid";
-        container.appendChild(div);
-    }
-}
-createGrid(16, 16);
+s;
