6c22a1191443e38e3c281be8d907e1fc22722335
Egor Kolesnik <egorisk1998@gmail.com>
2025-04-28 19:56:05 +0300

    made a function that creates a grid and now working on a drawing functions

--- scripts.js
+++ scripts.js
@@ -1,1 +1,40 @@
-s;
+const container = document.querySelector(".container");
+
+const grid = document.createElement("div");
+grid.className = "grid";
+container.appendChild(grid);
+
+function draw() {
+    let isDrawing = false;
+    const grid = document.querySelector(".grid");
+    grid.addEventListener("mousedown", () => {
+        isDrawing == true;
+        const cell = document.querySelector(".columnCell");
+        cell.addEventListener("mouseover", () => {
+            if (isDrawing) {
+                cell.style.backgroundcolor = "black";
+            }
+        });
+    });
+}
+
+function createGrid(num) {
+    for (let i = 0; i < num; i++) {
+        const row = document.createElement("div");
+        row.className = "rowCell";
+
+        for (let j = 0; j < num; j++) {
+            const column = document.createElement("div");
+            column.className = "columnCell";
+            row.appendChild(column);
+        }
+        grid.appendChild(row);
+    }
+}
+
+function drawingGame() {
+    createGrid(16);
+    draw();
+}
+
+drawingGame();
