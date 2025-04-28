49079bc38b02aeaf9cb6eb91f3a2e797e3ced14f
Egor Kolesnik <egorisk1998@gmail.com>
2025-04-28 22:53:11 +0300

    created a draw function and a button to change the size of the grid

--- scripts.js
+++ scripts.js
@@ -1,40 +1,54 @@
 const container = document.querySelector(".container");
 
-const grid = document.createElement("div");
-grid.className = "grid";
-container.appendChild(grid);
+const button = document.createElement("button");
+button.id = "changeSize";
+button.innerText = "Change grid size!";
+container.appendChild(button);
 
-function draw() {
-    let isDrawing = false;
-    const grid = document.querySelector(".grid");
-    grid.addEventListener("mousedown", () => {
-        isDrawing == true;
-        const cell = document.querySelector(".columnCell");
-        cell.addEventListener("mouseover", () => {
-            if (isDrawing) {
-                cell.style.backgroundcolor = "black";
-            }
-        });
-    });
-}
+button.addEventListener("click", () => {
+    const del = document.querySelector(".grid");
+    container.removeChild(del);
+    let input = prompt("choose a number between 1-100");
+
+    if (input < 1 || input > 100 || input == null) {
+        input = prompt("choose a number between 1-100");
+    } else {
+        createGrid(input);
+    }
+});
 
 function createGrid(num) {
+    const grid = document.createElement("div");
+    grid.className = "grid";
+    container.appendChild(grid);
     for (let i = 0; i < num; i++) {
         const row = document.createElement("div");
-        row.className = "rowCell";
+        row.className = "rowCell ";
 
         for (let j = 0; j < num; j++) {
             const column = document.createElement("div");
-            column.className = "columnCell";
+            column.className = "columnCell cell";
             row.appendChild(column);
         }
         grid.appendChild(row);
     }
-}
-
-function drawingGame() {
-    createGrid(16);
     draw();
 }
+function draw() {
+    let isDrawing = false;
+    const grid = document.querySelector(".grid");
 
-drawingGame();
+    grid.addEventListener("mousedown", () => {
+        isDrawing = true;
+    });
+    grid.addEventListener("mouseup", () => {
+        isDrawing = false;
+    });
+    grid.addEventListener("mouseover", (e) => {
+        if (isDrawing && e.target.classList.contains("columnCell")) {
+            e.target.style.backgroundColor = "black";
+        }
+    });
+}
+
+createGrid(16);
